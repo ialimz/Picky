3566ea526e27a8161e77ad3a05a307cf3a159958
Ali Moazenzadeh <alimz@divar.ir>
2019-07-10 09:24:11 +0430

    Initialize project

---
+++ Picky/Picky.h
@@ -0,0 +1,19 @@
+//
+//  Picky.h
+//  Picky
+//
+//  Created by Ali Moazenzadeh on 7/10/19.
+//  Copyright © 2019 DIvar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Picky.
+FOUNDATION_EXPORT double PickyVersionNumber;
+
+//! Project version string for Picky.
+FOUNDATION_EXPORT const unsigned char PickyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Picky/PublicHeader.h>
+
+
